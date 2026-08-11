23f99c61515b7a80074b246070dd65699b132391
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2026-08-11 10:27:45 +0100

    change nats consumer expiration time to unlimited

--- apps/backend/internal/workers/setup.go
+++ apps/backend/internal/workers/setup.go
@@ -103,8 +103,9 @@ func (ns *NATSJetStream) CreateStream(ctx context.Context, name string, subjects
 
 func (ns *NATSJetStream) CreateConsumer(ctx context.Context, stream, consumerName string) (jetstream.Consumer, error) {
 	consumer, err := ns.js.CreateOrUpdateConsumer(ctx, stream, jetstream.ConsumerConfig{
-		Name:      consumerName,
-		AckPolicy: jetstream.AckExplicitPolicy,
+		Name:              consumerName,
+		AckPolicy:         jetstream.AckExplicitPolicy,
+		InactiveThreshold: 0,
 	})
 	if err != nil {
 		return nil, err
